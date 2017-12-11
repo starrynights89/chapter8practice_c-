293bd0d6954b89f5c4419e4d7224b21b19c6c537
Alexander Hartson <alexander-hartson@live.com>
2017-12-11 12:14:04 -0600

    adding code

--- my.cpp
+++ my.cpp
@@ -1,2 +1,14 @@
 #include "my.h"
 #include "std_lib_facilities.h"
+
+int foo = 456;
+
+void print_foo()
+{
+    cout << "foo = " << foo << endl;
+}
+
+void print (int arg)
+{
+    cout << "= " << arg << endl;
+}
