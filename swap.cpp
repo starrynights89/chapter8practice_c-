dbfd333cb43551e4f18e222c914d76915ee0f9b8
Alexander Hartson <alexander-hartson@live.com>
2017-12-11 15:17:11 -0600

    adding code

--- swap.cpp
+++ swap.cpp
@@ -1,1 +1,22 @@
 #include "std_lib_facilities.h"
+
+void swap_v(int a, int b)
+{
+    int temp = a;
+    a = b;
+    b = temp;
+}
+
+void swap_r(int &a, int &b)
+{
+    int temp = a; 
+    a = b;
+    b = temp;
+}
+
+void swap_cr(const int &a, const int &b)
+{
+    int temp = a;
+    a = b;
+    b = temp; 
+}
