f7b920abf5b4bea75f8afb75e9fc647113fc7372
Alexander Hartson <alexander-hartson@live.com>
2017-12-12 16:06:49 -0600

    upload file

---
+++ namespace.cpp
@@ -0,0 +1,18 @@
+#include "std_lib_facilities.h"
+
+int main()
+{
+    X::var = 7;
+    X::print(); //print X's var
+    using namespace Y;
+    var = 9;
+    print(); //print Y's var
+    {
+        using Z::var;
+        using Z::print;
+        var = 11;
+        print(); //print Z's var
+    }
+    print(); //print Y's var
+    X::print(); //print X's var
+}
