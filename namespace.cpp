158632b7b9ffe303783a594d9ec51100e6f4098b
Alexander Hartson <alexander-hartson@live.com>
2017-12-12 16:11:37 -0600

    adding code

--- namespace.cpp
+++ namespace.cpp
@@ -1,5 +1,23 @@
 #include "std_lib_facilities.h"
 
+namespace X 
+{
+    int var = 0;
+    void print() {cout << "In space X: " << var << endl;}
+};
+
+namespace Y
+{
+    int var = 0;
+    void print() {cout << "In space Y: " << var << endl;}
+};
+
+namespace Z 
+{
+    int var = 0;
+    void print() {cout << "In space Z: " << var << endl;}
+};
+
 int main()
 {
     X::var = 7;
