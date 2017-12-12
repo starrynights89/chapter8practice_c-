7b57e50d2489897dd842e2863c4b5837c4ede525
Alexander Hartson <alexander-hartson@live.com>
2017-12-12 01:10:08 -0600

    modifying code

--- swap.cpp
+++ swap.cpp
@@ -2,24 +2,29 @@
 
 void swap_v(int a, int b)
 {
-    int temp = a;
+    int temp;
+    temp = a;
     a = b;
     b = temp;
 }
 
 void swap_r(int &a, int &b)
 {
-    int temp = a; 
+    int temp; 
+    temp = a; 
     a = b;
     b = temp;
 }
 
-void swap_cr(const int &a, const int &b)
+/*
+void swap_cr(const int & a, const int & b)
 {
-    int temp = a;
+    int temp;
+    temp = a; 
     a = b;
     b = temp; 
 }
+*/
 
 int main()
 {
@@ -27,7 +32,8 @@ int main()
     int y = 9;
     swap_v(x,y);
     cout << "swap_v(x,y) = " << x << " " << y << endl;
-    swap_r(x,y);
-    cout << "swap_r(x,y) = " << x << " " << y << endl;
-    
+    //swap_r(x,y);
+    //cout << "swap_r(x,y) = " << x << " " << y << endl;
+    keep_window_open();
+    return 0;
 }
